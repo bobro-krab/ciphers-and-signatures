7fe7dfc6f5d078c07c9d4407f163d7cc69c37517
bobro-krab <bobro-krab@bk.ru>
2015-12-25 00:05:58 +0600

    Loading graph complete.

---
+++ graph.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"strconv"
+)
+
+// ReadInts reads whitespace-separated ints from r. If there's an error, it
+// returns the ints successfully read so far as well as the error value.
+func ReadInts(r io.Reader) ([]int, error) {
+	scanner := bufio.NewScanner(r)
+	scanner.Split(bufio.ScanWords)
+	var result []int
+	for scanner.Scan() {
+		x, err := strconv.Atoi(scanner.Text())
+		if err != nil {
+			return result, err
+		}
+		result = append(result, x)
+	}
+	return result, scanner.Err()
+}
+
+func main() {
+	fmt.Println("Graph v0.1")
+
+	graphFilename := "input_graph"
+	graphFile, _ := os.Open(graphFilename)
+	defer graphFile.Close()
+
+	Ints, _ := ReadInts(graphFile)
+	fmt.Println(Ints)
+
+	return
+}
