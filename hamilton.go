465b1903f202ab5aebbd28955d6387520fb9a244
Mikhail Aleynikov <bobro-krab@bk.ru>
2015-12-26 17:39:55 +0600

    Seems that graphs only changing, not copying.

--- hamilton.go
+++ hamilton.go
@@ -14,8 +14,23 @@ type Alice struct {
 	F   graph.Graph // Encripted isomoprhic graph
 }
 
+func IsEdgeInCycle(e graph.Edge, cycle []int) bool {
+	for i := range cycle {
+		if i == 0 {
+			continue
+		}
+		if (e == graph.Edge{cycle[i], cycle[i-1]}) {
+			return true
+		}
+		if (e == graph.Edge{cycle[i-1], cycle[i]}) {
+			return true
+		}
+	}
+	return false
+}
+
 // Load graph, and setups another graphs(morphing and encryption)
-func (alice Alice) LoadGraph(filename string) {
+func (alice *Alice) LoadGraph(filename string) {
 	alice.rsa.Init()
 
 	// Loaded graph G
@@ -29,10 +44,13 @@ func (alice Alice) LoadGraph(filename string) {
 		alice.H.Edges[i].A += rand
 		alice.H.Edges[i].B += rand
 	}
+	for k := range alice.H.Cycle {
+		alice.H.Cycle[k] += rand
+	}
 	fmt.Println("Mutate graph:", alice.H)
 
 	// Enncrypt graph
-	alice.F = alice.G
+	alice.F = alice.H
 	for i := range alice.F.Edges {
 		alice.F.Edges[i].A = crypto.Pow(alice.F.Edges[i].A, alice.rsa.D, alice.rsa.N)
 		alice.F.Edges[i].B = crypto.Pow(alice.F.Edges[i].B, alice.rsa.D, alice.rsa.N)
@@ -42,13 +60,15 @@ func (alice Alice) LoadGraph(filename string) {
 }
 
 // Answer for question 1
-func (alice Alice) GetCycle() graph.Graph {
-	dict := map[graph.Edge]graph.Edge{}
-	var M graph.Graph
-	M = alice.F
-	for k := range M.Edges {
-		dict[M.Edges[k]] = alice.H.Edges[k]
-	}
+func (alice *Alice) GetCycle() graph.Graph {
+	M := alice.H
+	// for k := range M.Edges {
+	// 	if IsEdgeInCycle(M.Edges[k], M.Cycle) {
+	// 		continue
+	// 	}
+	// 	M.Edges[k].A = crypto.Pow(M.Edges[k].A, alice.rsa.D, alice.rsa.N)
+	// 	M.Edges[k].B = crypto.Pow(M.Edges[k].B, alice.rsa.D, alice.rsa.N)
+	// }
 	return M
 }
 
@@ -57,8 +77,6 @@ func main() {
 
 	var alice Alice
 	alice.LoadGraph("input_graph")
-	fmt.Println(alice.GetCycle())
-
 	fmt.Println(alice.GetCycle())
 	return
 }
